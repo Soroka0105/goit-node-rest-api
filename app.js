5cd707dc19ca3fc9ed12f6e5e23a5c02d24e5024
Soroka0105 <111576195+Soroka0105@users.noreply.github.com>
2024-02-06 12:06:23 -0500

    7

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", AuthRouter);
+app.use("/users", AuthRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
